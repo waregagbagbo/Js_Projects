aef0dc1d7a466eebc51a1d3d2192707d8f9b630f
unknown <gabelwarega@gmail.com>
2025-01-04 13:49:49 +0300

    updated the js logics to perfom the color flipper

--- Flipper/flipper.js
+++ Flipper/flipper.js
@@ -1,10 +1,24 @@
 // declare the array of the colors
 const colors = ['green','red','blue','yellow','purple',
-    'red','beige','marron','skyblue','blue','darkgreen',
+    'red','beige','maroon','skyblue','blue','darkgreen',
     'pink','brown']
 
 // fetch the HTML button class using DOM
-const btn =  document.getElementsByClassName('.btn');
+const btn =  document.getElementById('btns');
 
 // fetch the color dispaly section
-const colorFlavor = document.getElementById('#colors')
+const colorFlavor = document.getElementById('colors');
+
+btn.addEventListener('click', function(){
+    // generate a random color index from 0 to colors.length - 1
+    const randomColor = Math.floor(Math.random() * colors.length-1);
+    
+    // change the body background color
+    document.body.style.backgroundColor = colors[randomColor];
+
+    // display the color used
+    /*if(colorFlavor){
+        colorFlavor.textContent += `${colors[randomColor]}`
+        
+    }*/
+})
