8ea11077733301a81cc73cda6e86a46cad754cc2
unknown <gabelwarega@gmail.com>
2025-01-04 12:22:36 +0300

    Added the html files with contents

---
+++ Flipper/flipper.js
@@ -0,0 +1,10 @@
+// declare the array of the colors
+const colors = ['green','red','blue','yellow','purple',
+    'red','beige','marron','skyblue','blue','darkgreen',
+    'pink','brown']
+
+// fetch the HTML button class using DOM
+const btn =  document.getElementsByClassName('.btn');
+
+// fetch the color dispaly section
+const colorFlavor = document.getElementById('#colors')
