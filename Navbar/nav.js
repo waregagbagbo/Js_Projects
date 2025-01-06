5d838b2843cc1d0e6cca183ab09e31ad48693439
warega <warega@localhost.localdomain>
2025-01-06 16:36:46 +0300

    finalized on responsive nav bar

--- Navbar/nav.js
+++ Navbar/nav.js
@@ -1,9 +1,11 @@
 // fetch html elements via document elements for links class
-const link = document.querySelector('.links');
-const toggle = document.querySelector('.nav-toggle');
-//const spanning = document.querySelector('spans')
+const links = document.querySelector('.menu');
+const toggle = document.querySelector('.fa-bars');
+
+const image = document.getElementsByTagName('img');
+image.appendChild = "https://www.100jsprojects.com/project/navbar"
 
 // add it to the eventListener
 toggle.addEventListener('click', function() {
-    link.classList.toggle('show-links')
+    links.classList.toggle('show-menu')
 })
