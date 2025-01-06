342fa4de5b1b341eee73768738a64087859c63c1
warega <warega@localhost.localdomain>
2025-01-06 15:03:33 +0300

    modified the html file

--- Navbar/nav.js
+++ Navbar/nav.js
@@ -1,11 +1,9 @@
 // fetch html elements via document elements for links class
-const link = document.querySelector('links');
-const toggle = document.querySelector('nav-toggle');
+const link = document.querySelector('.links');
+const toggle = document.querySelector('.nav-toggle');
 //const spanning = document.querySelector('spans')
 
 // add it to the eventListener
-
-link.addEventListener('click', function(){
-    link.classList.toggle('toggle');    
+toggle.addEventListener('click', function() {
+    link.classList.toggle('show-links')
 })
-
