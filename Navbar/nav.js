54a9ac1119317f057acbbeb5204430bd3648edb0
unknown <gabelwarega@gmail.com>
2025-01-07 22:08:49 +0300

    new project

--- Navbar/nav.js
+++ Navbar/nav.js
@@ -1,11 +1,8 @@
 // fetch html elements via document elements for links class
-const link = document.querySelector('links');
-const toggle = document.querySelector('nav-toggle');
+const link = document.querySelector('.links');
+const toggle = document.querySelector('.nav-toggle');
 //const spanning = document.querySelector('spans')
 
-// add it to the eventListener
-
-link.addEventListener('click', function(){
-    link.classList.toggle('toggle');    
+toggle.addEventListener('click',function(){
+    toggle.classList.toggtle(link)
 })
-
