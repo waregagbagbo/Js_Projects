edf70574141ee1385e24488e70b0946300946a27
unknown <gabelwarega@gmail.com>
2025-01-08 23:54:16 +0300

    modified the js file with detailed logics

--- AgeCalculator/age.js
+++ AgeCalculator/age.js
@@ -4,7 +4,7 @@
 // Grab the html and return value as current age.
 
 // create a date object first
-const date = new Date();
+const dates = new Date();
 // create year
 const years = date.getFullYear();
 // create month
@@ -12,15 +12,38 @@ const months =  date.getMonth()+1;
 // create day
 const days = date.getDate();
 const fullDate = `${months}/${days}/${years}`
-console.log(fullDate)
 // grab form field
 const fom = document.querySelector('.forms');
 // grab the id for date
-const birthday = document.getElementById('birthday');
+const birthday = document.getElementById('birthday').value;
 
 // get result display
 const ageDisplay =  document.getElementById('ageDisplay');
 
+// let us handle form submission
 
-fom.addEventListener('click', ()=>{
+fom.addEventListener('submit', (event)=>{
+    event.preventDefault(); // Prevents default form submission
+
+    // grab birthday logics
+    if(!birthday){
+        ageDisplay.textContent = `Please enter a valid date? `
+        return;
+    }
+    const birth = new Date(birthday);
+
+    // calculate age
+    let age = dates.getFullYear() - birth.getFullYear();
+    const monthDifference =  dates.getMonth() -  birth.getMonth();
+
+    // adjust for months
+    if(
+        monthDifference < 0 || (monthDifference ===0 && dates.getDate() < birthday.getDate())
+         
+    ){
+        age--
+    }
+    // display the result
+        ageDisplay.textContent = `You are ${age} years old.`
+
 })
