a7eeb47b06881ec30b7a30471aa7d6821be7cfdf
unknown <gabelwarega@gmail.com>
2025-01-08 22:31:46 +0300

    modified js file

--- AgeCalculator/age.js
+++ AgeCalculator/age.js
@@ -3,3 +3,14 @@
 // Subtract current year fROM YOB
 // Grab the html and return value as current age.
 
+// create a date object first
+const date = new Date();
+// create year
+const years = date.getFullYear();
+// create month
+const months =  date.getMonth()+1;
+// create day
+const days = date.getDate();
+
+console.log(`${months} ${days} ${years}`)
+console.log(date)
