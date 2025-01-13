bb0e9fc501fca15358cbb236fd572e5af8df0589
unknown <gabelwarega@gmail.com>
2025-01-13 18:19:26 +0300

    added eventListeners to the form

--- TipCalculator/tip.js
+++ TipCalculator/tip.js
@@ -3,4 +3,13 @@ const tip = document.querySelector('.tips');
 tip.innerHTML= "Enter the bill amount and tip percentage to calculate the total."
 
 // section to display the totals
-const finalResult = document.getElementsById('final');
+const finalResult = document.getElementById('final');
+
+// grab the form content for logics
+const formLogic = document.getElementById('forms');
+
+// Perfom the logic by enable calculation of the result
+
+formLogic.addEventListener('click', (event) =>{
+    event.preventDefault();
+})
