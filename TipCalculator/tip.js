80bd4b12a42f345b3b1d5a9d17888ed126240cba
unknown <gabelwarega@gmail.com>
2025-01-13 17:23:52 +0300

    modified the css file on forms

--- TipCalculator/tip.js
+++ TipCalculator/tip.js
@@ -1,3 +1,6 @@
 // section for the paragraph
 const tip = document.querySelector('.tips');
 tip.innerHTML= "Enter the bill amount and tip percentage to calculate the total."
+
+// section to display the totals
+const finalResult = document.getElementsById('final');
