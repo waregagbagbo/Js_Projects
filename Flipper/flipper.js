12be57b7842d473795feb5ebb6a8049e47779e82
unknown <gabelwarega@gmail.com>
2025-01-15 00:02:04 +0300

    Modified the js file

--- Flipper/flipper.js
+++ Flipper/flipper.js
@@ -6,7 +6,7 @@ const colorChange =  document.getElementById('colorId');
 
 // initialize the set of color array
 const colors = ['beige','green','purple','blue','skyblue',
-    'maroon','pink','brown','darkgreen','cream','yellow']
+    'maroon','pink','brown','darkgreen','cream','yellow'];
 
 // grab the button iteme
 const ChangeBtn =  document.getElementById('changeColor')
@@ -14,8 +14,12 @@ const ChangeBtn =  document.getElementById('changeColor')
 // We will use EventListener to achieve logics in with respect to button
 ChangeBtn.addEventListener('click', function(){
     // let us randomly check for the color but let us loop through the color array
-    let randomColor = Math.floor(Math.random() * colors.length - 1);
+    let randomColor = Math.floor(Math.random() * colors.length);
 
     //enable change to the whole body
-    document.body.style.backgroundColor = colors[randomColor]
+    document.body.style.backgroundColor = colors[randomColor];
+    
+
+    // enable display of the color used:
+    colorChange.textContent = "The present color is: " + colors[randomColor].split(" ");
 })
