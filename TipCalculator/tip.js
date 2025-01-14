1c9bae5f4e86e7f79bd029d0b5c40add3d8c2c1a
unknown <gabelwarega@gmail.com>
2025-01-14 11:36:58 +0300

    added the logics to both js and html file

--- TipCalculator/tip.js
+++ TipCalculator/tip.js
@@ -10,6 +10,28 @@ const formLogic = document.getElementById('forms');
 
 // Perfom the logic by enable calculation of the result
 
-formLogic.addEventListener('click', (event) =>{
-    event.preventDefault();
+formLogic.addEventListener('submit', (event) =>{
+    event.preventDefault(); // prevents default form submission
+
+    // grab the bill logics
+    const getBill = document.getElementById('bill').value;
+    
+    // grab the percent logics
+    const tipPercent = document.getElementById('tipId').value;
+     
+    // find the percent
+    let percenResult = getBill *(tipPercent/100);
+
+    // find the actualResult
+    let actualResult = percenResult + getBill;
+
+    // display the result
+    finalResult.textContent = `The tip to pay is ${actualResult}`;
 })
+
+// create a function that handles this calculation
+// Inputs: Bill amount
+// let percent be any value
+// final result is: anyvalue multiplied by bill
+// add final result to actual output + bill amount
+// display the result.
