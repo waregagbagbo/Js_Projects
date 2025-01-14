0e2a3f1a10a64169cefc2a8e9ee6d22afc51335a
unknown <gabelwarega@gmail.com>
2025-01-14 12:57:54 +0300

    added the respective files with logic

---
+++ Flipper/flipper.js
@@ -0,0 +1,20 @@
+// Declare the set of color array
+// We will use random method to select the colors randomly
+
+// grab the color display upon change
+const colorChange =  document.getElementById('colorId');
+
+// initialize the set of color array
+const colors = ['beige','green','purple','blue','skyblue',
+    'maroon','pink','brown','darkgreen','cream','yellow']
+
+// grab the button iteme
+const ChangeBtn =  document.getElementById('changeColor')
+
+// We will use EventListener to achieve logics in with respect to button
+ChangeBtn.addEventListener('click', function(){
+    // let us randomly check for the color but let us loop through the color array
+    document.body.style.backgroundColor = 'beige';
+
+    
+})
