df980377c439603d4113a8f0f5c3f4a6d9dab719
warega <warega@localhost.localdomain>
2025-01-21 01:44:51 +0300

    adding the timer logics

--- Stopwatch/stop.js
+++ Stopwatch/stop.js
@@ -1,26 +1,37 @@
 // set the h1 color
-
 const h1 = document.getElementById('color');
 h1.style.color = 'brown';
 
-// fetch the date and time module
-const dates = new Date()
-console.log(dates.getHours())
-
 // grab the display result section
 const results = document.getElementById('result');
-results.textContent = dates
-
 
 // grab the start buttons
 const starts = document.getElementById('start');
-
 // stop button
 const stops = document.getElementById('stops');
-
 // restart
 const resets = document.getElementById('reset');
 
-// assign each button an eventlistener\
-// Use the date module in js
-// you can opt for a function
+// initialize time values
+let startTimer = 0;
+let elapsedTime = 0;
+let timeInterval;
+
+// function to startTimer
+function startTime() {
+    // set the date
+    startTimer = Date.now() - elapsedTime;
+
+    // set interval
+    timeInterval = serInterval(() => {
+        elapsedTime = Date.now() - startTime;
+        results.textContent = formatTime(elapsedTime);
+
+    }, 10);
+    starts.disabled = true;
+    stops.disabled = false;
+}
+
+
+// we will create function for time functions and tie each to an eventistener
+// we will create function for time functions and tie each to an eventistener
