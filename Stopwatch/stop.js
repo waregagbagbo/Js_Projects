b8fb98bd0a6743dcf7ee67ff4489de905fb0ecb4
warega <warega@localhost.localdomain>
2025-01-21 01:05:51 +0300

    pending css files

--- Stopwatch/stop.js
+++ Stopwatch/stop.js
@@ -1,4 +1,26 @@
 // set the h1 color
 
 const h1 = document.getElementById('color');
-h1.style.color = "maroon"
+h1.style.color = 'brown';
+
+// fetch the date and time module
+const dates = new Date()
+console.log(dates.getHours())
+
+// grab the display result section
+const results = document.getElementById('result');
+results.textContent = dates
+
+
+// grab the start buttons
+const starts = document.getElementById('start');
+
+// stop button
+const stops = document.getElementById('stops');
+
+// restart
+const resets = document.getElementById('reset');
+
+// assign each button an eventlistener\
+// Use the date module in js
+// you can opt for a function
