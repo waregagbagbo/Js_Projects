af9da80025976d5daa0eb07d482a6d8e23b592a8
warega <warega@localhost.localdomain>
2025-01-21 02:01:32 +0300

    added timer logics on the stopwatch app

--- Stopwatch/stop.js
+++ Stopwatch/stop.js
@@ -32,6 +32,42 @@ function startTime() {
     stops.disabled = false;
 }
 
+// set the function time format
+
+function formatTime(elapsedTime) {
+    let milliseconds = Math.floor((elapsedTime % 1000) / 10);
+    let seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
+    let minutes = Math.floor((elapsed % (1000 * 60 * 60)) / (1000 * 60));
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    return (
+        (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" +
+        (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" +
+        (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00") + "." +
+        (milliseconds > 9 ? milliseconds : "0" + milliseconds));
+}
+
+// function to stopTimer
+function stopsTimer() {
+    clearInterval(timeInterval);
+    starts.disabled = false;
+    stops.disabled = true;
+}
+// function to reseTimer
+function resetTimer() {
+    clearInterval(timeInterval);
+
+    elapsedTime = 0;
+    resets.textContent = "00:00:00";
+
+    starts.disabled = false;
+    stops.disabled = true;
+}
+
+// append these to EventListeners
+starts.addEventListener('click', startTimer);
+stops.addEventListener('click', stopsTimer);
+resets.addEventListener('click', resetTimer);
+
 
 // we will create function for time functions and tie each to an eventistener
 // we will create function for time functions and tie each to an eventistener
