f2ad26b6afcec0a42304bf2481cf070282ab66a2
warega <warega@localhost.localdomain>
2025-03-15 23:11:46 +0300

    Fixed the working of the game

--- Rock_Paper_Scissors/main.js
+++ Rock_Paper_Scissors/main.js
@@ -1,60 +1,53 @@
-function playGame(){
-  let humanScore = 0;
-  let computerScore = 0;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-// create a  function and set the global vars as args in the function
-function playRound(humanChoice,computerChoice){
- 
-  // human choice logic
-  const humanSelection = ()=>{
-    // make the humanchoice case insesitive
-    let makeChoice = prompt('Start the game. Key in preferences of ("rock","paper","scissors")')
-    return makeChoice.toLowerCase()
-  };
+    // create a  function and set the global vars as args in the function
+    function playRound(humanChoice, computerChoice) {
 
-  // computer choice
-  const computerSelection = () =>{
-    let computerChoice = ['rock','paper','scissors'];
-    let finalOutput  = Math.floor(Math.random() * computerChoice.length)
-    return computerChoice[finalOutput]
-  };  
+        // human choice logic
+        const humanSelection = () => {
+            // make the humanchoice case insesitive
+            let makeChoice = prompt('Start the game. Key in preferences of ("rock","paper","scissors")')
+            return makeChoice.toLowerCase()
+        };
 
-  // set the human and computer choices functions to variables
-  let humanChoice = humanSelection();
-  let computerChoice = computerSelection();
-  
-  // apply conditions
-  if(humanChoice === computerChoice){
-    return ("Equal")
-  }
-  else if(
-    (humanChoice === "rock" && computerChoice === "scissors") ||
-    (humanChoice === "scissors" && computerChoice === "paper") ||
-    (humanChoice === "paper" && computerChoice === "rock")
-  ){
-    humanScore += 1
-    return ("You win! " + humanChoice + " beats " + computerChoice)
-  }
-  else{
-    computerScore +=1;
-    return `You lose! ${computerChoice} beats ${humanChoice}`
-  }
+        // computer choice
+        const computerSelection = () => {
+            let computerChoice = ['rock', 'paper', 'scissors'];
+            let finalOutput = Math.floor(Math.random() * computerChoice.length)
+            return computerChoice[finalOutput]
+        };
+
+        // set the human and computer choices functions to variables
+        humanChoice = humanSelection();
+        computerChoice = computerSelection();
+
+        // apply conditions
+        if (humanChoice === computerChoice) {
+            return ("Equal")
+        } else if (
+            (humanChoice === "rock" && computerChoice === "scissors") ||
+            (humanChoice === "scissors" && computerChoice === "paper") ||
+            (humanChoice === "paper" && computerChoice === "rock")
+        ) {
+            humanScore += 1
+            return ("You win! " + humanChoice + " beats " + computerChoice)
+        } else {
+            computerScore += 1;
+            return `You lose! ${computerChoice} beats ${humanChoice}`
+        }
+    }
+    // return multiple rounds
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound())
+    }
+    // return the final score
+    console.log(`Human Score: ${humanScore} Computer Score: ${computerScore}`)
+    if (humanScore > computerScore) {
+        console.log("You win the game!");
+
+    }
 }
-// return multiple rounds
-for(let i=0; i < 5; i++){
-  console.log(playRound())
-}
-// return the final score
-console.log(`Human Score: ${humanScore} Computer Score: ${computerScore}`)
-if(humanScore > computerScore){
-  console.log("You win the game!");
 
-}
-}
-/*playGame()
-
-const maj = document.querySelector('.major')
-maj.addEventListener('click',playGame =>{
-  console.log('clicked')
-  playGame()
-})*/
+playGame()
