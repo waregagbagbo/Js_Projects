e15706e272e531033f1c038608104209fd1cadd1
warega <warega@localhost.localdomain>
2025-03-27 22:35:23 +0300

    modified js file

--- Rock_Paper_Scissors/main.js
+++ Rock_Paper_Scissors/main.js
@@ -7,60 +7,59 @@ const scores = document.querySelector('.score');
 const reset = document.querySelector('.reset');
 
 // Add the listeners for the buttons
-rocks.addEventListener('click', function(){
- // console.log('rock')
-  playRound('rock')
+rocks.addEventListener('click', function() {
+    // console.log('rock')
+    playRound('rock')
 });
 
-papers.addEventListener('click', function(){
-  //console.log('paper')
-  playRound('paper')
+papers.addEventListener('click', function() {
+    //console.log('paper')
+    playRound('paper')
 });
 
-scissor.addEventListener('click', function(){
-  //console.log('scissors')
-  playRound('scissors')
+scissor.addEventListener('click', function() {
+    //console.log('scissors')
+    playRound('scissors')
 });
 
 
 // Global variables
-  let humanScore = 0;
-  let computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
 // create a  function and set the global vars as args in the function
-function playRound(humanChoice,computerChoice){
- 
-  // human choice logic
-  const humanSelection = ()=>{
-    return humanChoice;
-  }
+function playRound(humanChoice, computerChoice) {
 
-  // computer choice
-  const computerSelection = () =>{
-    let computerSelector = ['rock','paper','scissors'];
-    let finalOutput  = Math.floor(Math.random() * computerSelector.length)
-    return computerSelector[finalOutput]
-  };  
+    // human choice
+    function humanSelection(choice) {
+        document.getElementById('results').innerText += choice;
+        return choice
 
-  // set the human and computer choices functions to variables
-  let humanTake = humanSelection();
-  computerChoice = computerSelection();
-  
-  // apply conditions
-  if(humanTake === computerChoice){
-    return ("Equal")
-  }
-  else if(
-    (humanTake === "rock" && computerChoice === "scissors") ||
-    (humanTake === "scissors" && computerChoice === "paper") ||
-    (humanTake === "paper" && computerChoice === "rock")
-  ){
-    humanScore += 1
-    return ("You win! " + humanTake + " beats " + computerChoice)
-  }
-  else{
-    computerScore +=1;
-    return `You lose! ${computerChoice} beats ${humanTake}`
-  }
-}
-playRound()
+    }
+
+    // computer choice
+    function computerSelection() {
+        let choices = ['rock', 'paper', 'scissors']
+        let finalOutput = Math.floor(Math.random() * choices.length);
+        return choices[finalOutput]
+    }
+
+    // set the human and computer choices functions to variables
+    humanChoice = humanSelection();
+    computerChoice = computerSelection();
+
+    // apply conditions
+    if (humanChoice === computerChoice) {
+        return ("Equal")
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "scissors" && computerChoice === "paper") ||
+        (humanChoice === "paper" && computerChoice === "rock")
+    ) {
+        humanScore += 1
+        return ("You win! " + humanChoice + " beats " + computerChoice)
+    } else {
+        computerScore += 1;
+        return `You lose! ${computerChoice} beats ${humanChoice}`
+    }
+};
