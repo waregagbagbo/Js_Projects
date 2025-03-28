e5697b09393d9d9f678bccc8863bde76c06705cd
unknown <gabelwarega@gmail.com>
2025-03-29 00:20:46 +0300

    modified files

--- Rock_Paper_Scissors/main.js
+++ Rock_Paper_Scissors/main.js
@@ -8,7 +8,6 @@ const reset = document.querySelector('.reset');
 
 // Add the listeners for the buttons
 rocks.addEventListener('click', function(){
- // console.log('rock')
   playRound('rock')
 });
 
@@ -32,7 +31,8 @@ function playRound(humanChoice,computerChoice){
  
   // human choice logic
   const humanSelection = ()=>{
-    return humanChoice;
+    let humanTake = prompt('Start game')
+    return humanTake.toLowerCase()
   }
 
   // computer choice
@@ -43,24 +43,23 @@ function playRound(humanChoice,computerChoice){
   };  
 
   // set the human and computer choices functions to variables
-  let humanTake = humanSelection();
+  humanChoice = humanSelection();
   computerChoice = computerSelection();
   
   // apply conditions
-  if(humanTake === computerChoice){
+  if(humanChoice === computerChoice){
     return ("Equal")
   }
   else if(
-    (humanTake === "rock" && computerChoice === "scissors") ||
-    (humanTake === "scissors" && computerChoice === "paper") ||
-    (humanTake === "paper" && computerChoice === "rock")
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "scissors" && computerChoice === "paper") ||
+    (humanChoice === "paper" && computerChoice === "rock")
   ){
     humanScore += 1
-    return ("You win! " + humanTake + " beats " + computerChoice)
+    return ("You win! " + humanChoice+ " beats " + computerChoice)
   }
   else{
     computerScore +=1;
-    return `You lose! ${computerChoice} beats ${humanTake}`
+    return `You lose! ${computerChoice} beats ${humanChoice}`
   }
 }
-playRound()
